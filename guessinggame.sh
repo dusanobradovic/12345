791324f616f62fe4be6fd65ab5ec682b87efde3e
dusanobradovic <51491594+dusanobradovic@users.noreply.github.com>
2020-10-02 19:59:00 +0200

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
+myNumber=$[($RANDOM % 100)+1]
+echo "Enter number between 1 and 100: "
 
-n1=$[($RANDOM % 100) +1]
-guesses=1
-echo -n "I'm thinking of a number between 1 and 100. Your guess:"
+while read yourNumber;
+do
 
-while read n2; do
+if  [ $yourNumber -eq $myNumber ];
+then 
+echo -n ''Good job.'';
 
-if   [[ $n2 -eq $n1 ]]; then
-break;  
 else
-echo    
-if [[ $n2 -gt $n1 ]]; then 
-echo -n "Sorry, your guess is too high. New guess:"
-elif [[ $n2 -lt $n1 ]]; then
-echo -n "Sorry, your guess is too low. New guess:"
-fi      
+if [ $yourNumber -gt $myNumber ]; 
+then 
+echo -n "Your guess is too high. Try again."
+
+elif [ $yourNumber -lt $myNumber ]; 
+then
+echo -n "Your guess is too low. Try again."
 fi
-guesses=$((guesses+1))
+fi 
 
 done
-echo
-echo "Good job! It took you $guesses guesses to get the right number."
