b8b0213020b8c0563f8044d87d3258566795d237
dusanobradovic <51491594+dusanobradovic@users.noreply.github.com>
2020-10-02 17:45:10 +0200

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,39 +1,44 @@
 import random
-import math
- # Taking Inputs
-lower = int(input("Emter Lower bound:- ")) 
+
+ guessesTaken = 0
+
+
+
+print('Hello! What is your name?')
+
+myName = input()
+
+
+
+number = random.randint(1, 20)
+
+ print('Well, ' + myName + ', I am thinking of a number between 1 and 20.')
+
+
+
+ while guessesTaken < 6:
+ print('Take a guess.') # There are four spaces in front of print.
  
-# Taking Inputs
-upper = int(input("Enter Upper bound:- "))  
- 
-# generating random number between
-# the lower and upper
-x = random.randint(lower, upper)
-print("\n\tYou've only ", round(math.log(upper - lower + 1, 2))," chances to guess the integer!\n")
- 
- # Initializing the number of guesses.
-count = 0
- 
-# for calculation of minimum number of
-# guesses depends upon range
-while count < math.log(upper - lower + 1, 2):
-    count += 1
-     
-     # taking guessing number as input
-    guess = int(input("Guess a number:- ")) 
-     
-    # Condition testing
-    if x == guess:  
-       print("Congratulations you did it in ", count, " try")
-       # Once guessed, loop will break 
-       break
-    elif x > guess:
-       print("You guessed too small!")
-    elif x < guess:
-       print("You Guessed too high!")
- 
-# If Guessing is more than required guesses, 
-# shows this output.
-if count >= math.log(upper - lower + 1, 2):
-   print("\nThe number is %d"%x)
-   print("\tBetter Luck Next time!")
+guess = input()
+guess = int(guess)
+
+guessesTaken = guessesTaken + 1
+
+if guess < number:
+print('Your guess is too low.') # There are eight spaces in front of print.
+
+if guess > number:
+print('Your guess is too high.')
+
+if guess == number:
+ break
+
+if guess == number:
+ guessesTaken = str(guessesTaken)
+ print('Good job, ' + myName + '! You guessed my number in ' + guessesTaken + ' guesses!')
+
+
+if guess != number:
+number = str(number)
+
+ print('Nope. The number I was thinking of was ' + number)
